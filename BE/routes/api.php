b3bb0d7ce7dba60cec6e263ff7a83d9e5de89d40
BlitzCry <132849977+BlitzCry@users.noreply.github.com>
2024-03-10 12:01:46 +0000

    add healthehck on pipelines

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Carbon;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,6 +19,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("healthcheck", function(Request $request) {
+Route::get("healthcheck", function (Request $request) {
     return response()->json(["health" => "OK"]);
+});
+
+Route::group(["prefix" => "pipeline/"], function () {
+    Route::get("healthcheck", function (Request $request) {
+        return response()->json(["health" => "OK", "routes" => "pipeline"]);
+    });
+
+    Route::get("date", function (Request $request) {
+        dd($request->date);
+        return Carbon::parse($request->date);
+    });
+
+    Route::get("time", function (Request $request) {
+        return Carbon::parse($request->time);
+    });
 });
