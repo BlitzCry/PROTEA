13df6bce1fc31ce277937179dc3239c418c4853b
BlitzCry <132849977+BlitzCry@users.noreply.github.com>
2024-03-10 11:51:08 +0000

    add healthechck API

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Carbon;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("healthcheck", function(Request $request) {
+    return response()->json(["health" => "OK"]);
 });
